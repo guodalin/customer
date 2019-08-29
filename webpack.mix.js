aed50cbf9c0e9384c73326f908e63e4325017b17
欧军辉 <oujh@91360.com>
2019-08-29 16:27:37 +0800

    auto complie package resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const fs = require('fs');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,8 +13,61 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.setPublicPath('public')
-    .setResourceRoot('../') // turns assets paths in css relative to css file
+mix.setPublicPath('public');
+
+const packagesFolder = './packages';
+const depth = 2; // vender(1)/packagename(2)
+
+/**
+ * 遍历目录
+ *
+ * @param {String} p
+ * @param {Number} depth
+ */
+const walk = function (p, deep) {
+    if (deep > depth) {
+        return true;
+    }
+
+    let list = fs.readdirSync(p).filter(function (f) {
+        return fs.statSync(path.resolve(p, f)).isDirectory();
+    });
+
+    // found
+    if (deep === depth) {
+        list.forEach(function (packageName) {
+            const jsFolder = path.resolve(p, packageName, 'resources', 'js');
+            const cssFolder = path.resolve(p, packageName, 'resources', 'scss');
+
+            files(jsFolder, 'js').forEach(function (js) {
+                mix.js(path.resolve(jsFolder, js), path.resolve('vendor', packageName, 'js', js));
+            });
+
+            files(cssFolder, 'scss').forEach(function (scss) {
+                mix.sass(path.resolve(cssFolder, scss), path.resolve('vendor', packageName, 'css', path.basename(scss, path.extname(scss)) + '.css'));
+            });
+        });
+    } else {
+        list.forEach(function (d) {
+            walk(path.resolve(p, d), deep + 1);
+        });
+    }
+};
+
+const files = function (dir, ext) {
+    if (!fs.existsSync(dir)) {
+        return [];
+    }
+
+    return fs.readdirSync(dir).filter(function (f) {
+        return fs.statSync(path.resolve(dir, f)).isFile() && (path.extname(f).toLocaleLowerCase() === ('.' + ext).toLocaleLowerCase());
+    });
+};
+
+walk(packagesFolder, 1);
+
+mix
+    // .setResourceRoot('../') // turns assets paths in css relative to css file
     .sass('resources/sass/frontend/app.scss', 'css/frontend.css')
     .sass('resources/sass/backend/app.scss', 'css/backend.css')
     .js('resources/js/frontend/app.js', 'js/frontend.js')
