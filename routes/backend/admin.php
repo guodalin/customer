33356fdeaaeae7d5b64109b2ad6aa06a55a75f89
欧军辉 <oujh@91360.com>
2019-09-07 11:04:35 +0800

    optimize routes file

--- routes/backend/admin.php
+++ routes/backend/admin.php
@@ -9,16 +9,16 @@ Route::redirect('/', '/admin/dashboard', 301);
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 // Menu actions
+Route::post('menu/item', [MenuItemController::class, 'store']);
+Route::put('menu/item/{item}', [MenuItemController::class, 'update']);
+Route::delete('menu/item/{item}', [MenuItemController::class, 'destroy']);
+Route::put('menu/item/{item}/swap/{direction}', [MenuItemController::class, 'swap']);
+
 Route::get('menu', [MenuController::class, 'index'])->name('menu.index');
 Route::post('menu', [MenuController::class, 'store']);
 Route::delete('menu/{menu}', [MenuController::class, 'destroy']);
 Route::put('menu/{menu}', [MenuController::class, 'update']);
 Route::get('menu/{menu}', [MenuController::class, 'show']);
-
-Route::post('menu/item', [MenuItemController::class, 'store']);
-Route::put('menu/item/{item}', [MenuItemController::class, 'update']);
-Route::delete('menu/item/{item}', [MenuItemController::class, 'destroy']);
-Route::put('menu/item/{item}/swap/{direction}', [MenuItemController::class, 'swap']);
 
 Route::resource('experts', 'ExpertController');
 Route::resource('hospitals', 'ExpertController');
