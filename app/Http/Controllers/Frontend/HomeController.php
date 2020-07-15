679e4e1f57b579d6b4b99a8dd05d1117613b7952
Guo Dalin <376244558@qq.com>
2020-07-15 23:25:50 +0800

    edit

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -22,13 +22,12 @@ class HomeController extends Controller
     public function qrcode()
     {
         $customers = Customer::get()->toArray();
-        $rand = rand(1, count($customers));
+        $rand = rand(0, count($customers)-1);
         if (!isset($_COOKIE['rand'])) {
             setcookie('rand', $rand);
         } else {
             $rand = $_COOKIE['rand'];
         }
-
         $customer = isset($customers[$rand]) ? $customers[$rand] : ['name' => '官方客服','avatar' => 'http://rand.test/storage/avatars/vMA5mj6FiMtM5Pnf0EuBhF2NpCPKau8eN1IZidm6.png'];
         return view('frontend.qrcode',compact('customer'));
     }
