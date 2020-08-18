52deb61d105602cd5adba3de524103dc099ebac2
guodalin <guodl@91360.com>
2020-08-18 15:09:01 +0800

    修改后台bug

--- app/Http/Controllers/Backend/CustomerController.php
+++ app/Http/Controllers/Backend/CustomerController.php
@@ -87,14 +87,14 @@ class CustomerController extends Controller
      */
     public function update(Request $request, customer $customer)
     {
-        $path = $request->file('avatar')->store('avatars');
+        if ($request->file('avatar')) {
+            $path = $request->file('avatar')->store('avatars');
+            $customer->avatar = $path;
+        }
+        $customer->name = $request->input('name');
+        $customer->info = $request->input('info');
 
-        $data = [
-            'name' => $request->input('name'),
-            'info' => $request->input('info'),
-            'avatar' => $path
-        ];
-        $customer->update($data);
+        $customer->save();
 
         return redirect()->route('admin.customer.index')->withFlashSuccess('修改成功');
     }
